df9a7e4248c362785a79001a90d6bab1819ce9b5
Nemanja Malocic <n.malocic@vegait.rs>
2021-12-24 12:52:41 +0100

    Added new database script

---
+++ api/db/04_new_kindergardens_removed_non_NoviSad.sql
@@ -0,0 +1,8 @@
+﻿UPDATE kindergarden
+SET IsActive = 0
+WHERE department NOT LIKE N'ПУ Радосно детињство'
+
+INSERT INTO kindergarden (city, municipality, government, department, name, street, street_number, postal_code, location_type, IsActive) VALUES
+(N'Нови Сад',N'Нови Сад',N'Нови Сад',N'ПУ Радосно детињство',N'СРНА',N'Радоја Домановића',N'24',21242,1, 1),
+(N'Нови Сад',N'Нови Сад',N'Нови Сад',N'ПУ Радосно детињство',N'МЕДА',N'Радничка',N'47',21242,1, 1),
+(N'Нови Сад',N'Нови Сад',N'Нови Сад',N'ПУ Радосно детињство',N'КРЦКО ОРАШЧИЋ',N'Орахова',N'3',21242,1, 1)
